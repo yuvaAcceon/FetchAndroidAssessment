c280c223ecb5a4e7645378cc56b16b87e34d38ed
yuvasaigunda <yuvasaigunda1@gmail.com>
2024-03-05 13:02:41 -0500

    Added cards for ListItems

--- app/src/main/java/com/yuvasai/yuvasaiandroidassessment/presentation/list_items/components/ListItem.kt
+++ app/src/main/java/com/yuvasai/yuvasaiandroidassessment/presentation/list_items/components/ListItem.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -17,32 +18,34 @@ import com.yuvasai.yuvasaiandroidassessment.domain.model.FetchListItem
 fun ListItem(
     listItem: FetchListItem
 ) {
-    Row(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(20.dp),
-        horizontalArrangement = Arrangement.SpaceBetween
-    ) {
-        Text(
-            text = "${listItem.listId}",
-            style = MaterialTheme.typography.bodyMedium,
-            overflow = TextOverflow.Ellipsis,
-            textAlign = TextAlign.Center,
-            modifier = Modifier.weight(1F)
-        )
-        Text(
-            text = "${listItem.name}",
-            style = MaterialTheme.typography.bodyMedium,
-            overflow = TextOverflow.Ellipsis,
-            textAlign = TextAlign.Center,
-            modifier = Modifier.weight(1F)
-        )
-        Text(
-            text = "${listItem.id}",
-            style = MaterialTheme.typography.bodyMedium,
-            overflow = TextOverflow.Ellipsis,
-            textAlign = TextAlign.Center,
-            modifier = Modifier.weight(1F)
-        )
+    Card {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(20.dp),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Text(
+                text = "${listItem.listId}",
+                style = MaterialTheme.typography.bodyMedium,
+                overflow = TextOverflow.Ellipsis,
+                textAlign = TextAlign.Center,
+                modifier = Modifier.weight(1F)
+            )
+            Text(
+                text = "${listItem.name}",
+                style = MaterialTheme.typography.bodyMedium,
+                overflow = TextOverflow.Ellipsis,
+                textAlign = TextAlign.Center,
+                modifier = Modifier.weight(1F)
+            )
+            Text(
+                text = "${listItem.id}",
+                style = MaterialTheme.typography.bodyMedium,
+                overflow = TextOverflow.Ellipsis,
+                textAlign = TextAlign.Center,
+                modifier = Modifier.weight(1F)
+            )
+        }
     }
 }
--- app/src/main/java/com/yuvasai/yuvasaiandroidassessment/presentation/list_items/components/ListItemsScreen.kt
+++ app/src/main/java/com/yuvasai/yuvasaiandroidassessment/presentation/list_items/components/ListItemsScreen.kt
@@ -4,9 +4,11 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.CircularProgressIndicator
@@ -62,6 +64,11 @@ fun ListItemsScreen(
             LazyColumn(modifier = Modifier.fillMaxSize()) {
                 items(state.listItems) { listItem ->
                     ListItem(listItem = listItem)
+                    Spacer(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(5.dp)
+                    )
                 }
             }
             if (state.error.isNotBlank()) {
